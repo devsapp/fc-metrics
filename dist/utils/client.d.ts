0f95df238e13b5bed8dc9912b162987174f6989f
wss-git <wssgryx@163.com>
2021-09-16 11:15:20 +0800

    refactor: 0.1.7

--- dist/utils/client.d.ts
+++ dist/utils/client.d.ts
@@ -1,5 +1,6 @@
+import Core from '@alicloud/pop-core';
 import { ICredentials } from '../interface';
 export declare const getFcClient: (credentials: ICredentials, region: string) => Promise<any>;
-export declare const getCmsClient: (credentials: ICredentials) => any;
+export declare const getCmsClient: (credentials: ICredentials) => Core;
 export declare const getSLSClient: (credentials: ICredentials, region: string) => any;
-export declare const getTraceClicnt: (credentials: ICredentials, region: string) => any;
+export declare const getTraceClicnt: (credentials: ICredentials, region: string) => Core;
