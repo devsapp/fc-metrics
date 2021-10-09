f98002846a01a43de893008013091dedebcf829f
wss-git <wssgryx@163.com>
2021-10-09 16:51:07 +0800

    fix: not found static

--- src/script/build.sh
+++ src/script/build.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
 echo '编译静态页面'
-cd ./src/ui
-npm i && npm run build
 
-cd ../
-echo '清除缓存'
-
-
-echo '将项目文件 cp 到部署目录'
-mkdir -p ../dist/utils/static
-cp -r ui/dist/static/  ../dist/utils/static
+cd ./src/ui && npm i && npm run build && cd ../ && mkdir -p ../dist/utils && cp -r ui/dist/static/  ../dist/utils/static/
